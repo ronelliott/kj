744c7865bfad31fb784e6d214ce66777ebc8559f
Ron Elliott <ron@elliott.im>
2015-08-18 11:58:36 -0700

    Removed some throws, fixed some issues, added more tests.

--- test/app/handle.js
+++ test/app/handle.js
@@ -11,6 +11,14 @@ describe('handle', function() {
         this.res = { end: sinon.spy(), on: sinon.spy(), write: sinon.spy() };
     });
 
+    it('should throw an error if no router is defined', function() {
+        var self = this;
+        this.app.router = null;
+        (function() {
+            self.app.handle();
+        }).should.throw('Router not defined!');
+    });
+
     it('should set the status to 404 if no handler processed the request', function(done) {
         this.app.handle(this.req, this.res, function(res) {
             res.statusCode.should.equal(404);
@@ -26,6 +34,36 @@ describe('handle', function() {
         this.app.get('/', h1, h2);
         this.app.handle(this.req, this.res, function(err) {
             err.should.equal('woops!');
+            called.should.equal(true);
+            h2.called.should.equal(false);
+            done();
+        });
+    });
+
+    it('should set the status to 500 if the status is 200 when a handler passes an error to next', function(done) {
+        var self = this,
+            called = false,
+            h1 = function(next) { called = true; next('woops!'); },
+            h2 = sinon.spy();
+
+        this.app.get('/', h1, h2);
+        this.app.handle(this.req, this.res, function(err) {
+            err.should.equal('woops!');
+            called.should.equal(true);
+            h2.called.should.equal(false);
+            self.res.statusCode.should.equal(500);
+            done();
+        });
+    });
+
+    it('should stringify the object if next is called in a handler with an object', function(done) {
+        var called = false,
+            h1 = function(next) { called = true; next({ woops: true }); },
+            h2 = sinon.spy();
+
+        this.app.get('/', h1, h2);
+        this.app.handle(this.req, this.res, function(err) {
+            err.should.equal('{"woops":true}');
             called.should.equal(true);
             h2.called.should.equal(false);
             done();
@@ -53,6 +91,33 @@ describe('handle', function() {
         this.app.handle(this.req, this.res, function(ctx) {
             ctx('foo').should.equal('bar');
             called.should.equal(true);
+            done();
+        });
+    });
+
+    it('should resolve the handler if it is a string', function(done) {
+        var h1 = sinon.spy();
+
+        this.app.caller.resolver.add('foo', h1);
+        this.app.get('/', { handler: 'foo' });
+        this.app.handle(this.req, this.res, function() {
+            h1.called.should.equal(true);
+            done();
+        });
+    });
+
+    it('should return an error if the handler is not defined in the route', function(done) {
+        this.app.get('/', null);
+        this.app.handle(this.req, this.res, function(err) {
+            err.toString().should.equal('Error: Undefined handler for route: /');
+            done();
+        });
+    });
+
+    it('should return an error if the handler is not defined in the resolver', function(done) {
+        this.app.get('/', { handler: 'foo' });
+        this.app.handle(this.req, this.res, function(err) {
+            err.toString().should.equal('Error: Undefined handler: {"handler":"foo"}');
             done();
         });
     });
